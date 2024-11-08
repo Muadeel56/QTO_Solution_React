4aacf5d7c7e5d6d1967b1e243ac42fe486400e95
Muadeel56 <muadeel59@gmail.com>
2024-11-08 12:51:31 +0500

    Make Home Page title section

---
+++ qtoSolutionReact/src/pages/HomePage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeTitle from '../../components/HomeTitle'
+
+function HomePage() {
+  return (
+    <div>
+        <HomeTitle />
+    </div>
+  )
+}
+
+export default HomePage
