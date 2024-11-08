638985c83df442e04f69bb3b18455c29e14f3231
Muadeel56 <muadeel59@gmail.com>
2024-11-08 12:58:50 +0500

    Make stats component

--- qtoSolutionReact/src/pages/HomePage/index.jsx
+++ qtoSolutionReact/src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeTitle from '../../components/HomeTitle'
+import Stats from '../../components/Stats'
 
 function HomePage() {
   return (
     <div>
         <HomeTitle />
+        <Stats />
     </div>
   )
 }
